454987c93a186d9d6abf3161a72e006ac96f9829
tarami6 <allindn@gmail.com>
2019-02-12 08:49:47 +0200

    bootsratp test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {  Button } from 'react-bootstrap';
+
 
 class App extends Component {
   render() {
     return (
         <div>
-        Rami Testing
+            <Button variant="primary">Primary</Button>
         </div>
     );
   }
