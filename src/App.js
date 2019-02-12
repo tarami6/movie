f0014fd9e2051f8e33abe4aac5a94beb51fb5527
tarami6 <allindn@gmail.com>
2019-02-12 09:50:40 +0200

    test axios

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 // Bootsratp
 import {Button} from 'react-bootstrap';
+import axios from 'axios';
 // Redux
 import {connect} from 'react-redux'
 import {deleteMovie, editMovie, initialMovies} from "./redux/actions/MovieActions";
@@ -20,23 +21,26 @@ class App extends Component {
     }
 
     componentWillMount() {
-         this.fetchMovies();
+        this.fetchMovies();
     }
-    // Movie Api
-    fetchMovies = async () => {
-        try {
-            const response = await fetch(`http://api.tvmaze.com/search/shows?q=game`);
-            const responseJson = await response.json();
-            await this.props.initialMovies(responseJson)
-            await this.setState({loading: false})
 
-        } catch (e) {
-            console.log('erroe', e);
-        }
-    };
+    // Movie Api
+    fetchMovies = () => {
+
+        axios.get(`http://api.tvmaze.com/search/shows?q=game`)
+            .then(res => {
+                const movies = res.data;
+                console.log("m", movies)
+                this.props.initialMovies(movies)
+                this.setState({loading: false})
+            })
+
+
+    }
+
 
     render() {
-        if(!this.state.loading){
+        if (!this.state.loading) {
             console.log("test", this.props.movie.movies[0].show.name)
         } else {
             console.log("test load failed")
@@ -44,10 +48,9 @@ class App extends Component {
 
         return (
             <div>
-                {!this.state.loading? <p>{this.props.movie.movies[0].show.name}</p>:
+                {!this.state.loading ? <p>{this.props.movie.movies[0].show.name}</p> :
                     <p>No fetch</p>
                 }
-
 
 
                 <Button variant="primary">Primary</Button>
